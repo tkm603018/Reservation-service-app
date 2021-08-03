2fa6df7178cd32ec9ddc1a11f438d901be667546
tkm603018 <tkm603018@gmail.com>
2021-08-03 11:55:13 +0900

    Create app/models/reservation_frame.rb

---
+++ app/models/reservation_frame.rb
@@ -0,0 +1,6 @@
+class ReservationFrame < ApplicationRecord
+  belongs_to :planner
+  belongs_to :time_frame
+  validates :planner_id, uniqueness: { scope: :time_frame_id }
+  has_many :reservations, dependent: :destroy
+end
