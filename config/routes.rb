29de4e79316cb696ad536d5bd220c3640341a789
tkm603018 <51039761+tkm603018@users.noreply.github.com>
2021-08-03 15:00:46 +0900

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  #Client側
   resources :reservations, only: [:index, :new]
   resources :reservation_frames, only: [:index, :new]
   resource :clients, only: [:show, :new, :create]
   resources :planners, only: [:index, :show, :new, :create]
-
-  get 'reservations', to: 'reservations#index'
-  get 'clients/sign_up', to: 'clients#create'
-  get 'clients/sign_in', to: 'clients#new'
-  get 'clients/profile', to: 'clients#show'
-  get 'planners/index', to: 'planners#index'
-  get 'planners/:id', to: 'planners#show'
-  get 'reservations/new', to: 'reservations#new'
-  
-  #Planner側
-  get 'reservation_frames', to: 'reservation_frames#index'
-  get 'planners/sign_up', to: 'planners#create'
-  get 'planners/sign_in', to: 'planners#new'
-  get 'planners/profile', to: 'planners#show'
-  get 'reservation_frames/new', to: 'reservation_frames#new'
-
 end
