370f01f1c758fb93107da02c3f5ca3a89117f5ee
tkm603018 <51039761+tkm603018@users.noreply.github.com>
2021-08-03 13:17:02 +0900

    Update app/models/reservation_frame.rb
    制約の種類ごとに一行ずつを開ける

--- app/models/reservation_frame.rb
+++ app/models/reservation_frame.rb
@@ -1,6 +1,8 @@
 class ReservationFrame < ApplicationRecord
   belongs_to :planner
   belongs_to :time_frame
+  
   validates :planner_id, uniqueness: { scope: :time_frame_id }
+  
   has_many :reservations, dependent: :destroy
 end
