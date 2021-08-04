f3a57757f67e734fa1b8f50878a670757f2a859e
tkm603018 <tkm603018@gmail.com>
2021-08-04 15:49:42 +0900

    Update app/helpers/planners_helper.rb

--- app/helpers/planners_helper.rb
+++ app/helpers/planners_helper.rb
@@ -1,2 +1,24 @@
 module PlannersHelper
+  def sign_in(planner)
+    session[:planner_id] = planner.id
+  end
+
+  def current_planner
+    if session[:planner_id]
+      @current_planner ||= Planner.find_by(id: session[:planner_id])
+    end
+  end
+
+  def current_planner?(planner)
+    planner == current_planner
+  end
+
+  def signed_in?
+    !current_planner.nil?
+  end
+
+  def sign_out
+    session.delete(:planner_id)
+    @current_planner = nil
+  end
 end
