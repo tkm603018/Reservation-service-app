d22d2c93e2deae15ac644dc423e2ef2898eafd2f
tkm603018 <tkm603018@gmail.com>
2021-08-04 13:58:15 +0900

    Update app/helpers/clients_helper.rb

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1,2 +1,24 @@
 module ClientsHelper
+  def sign_in(client)
+    session[:client_id] = client.id
+  end
+
+  def current_client
+    if session[:client_id]
+      @current_client ||= Client.find_by(id: session[:client_id])
+    end
+  end
+
+  def current_client?(client)
+    client == current_client
+  end
+
+  def signed_in?
+    !current_client.nil?
+  end
+
+  def sign_out
+    session.delete(:client_id)
+    @current_client = nil
+  end
 end
