c28a5b1cea22b396d9a664cc25e5dcd891c6221e
tkm603018 <tkm603018@gmail.com>
2021-08-04 13:57:37 +0900

    Update app/controllers/clients_controller.rb

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,4 +1,7 @@
 class ClientsController < ApplicationController
+  include ClientsHelper
+  before_action :signed_in_client, only:[:destroy]
+
   def index
   end
 
@@ -9,5 +12,37 @@ class ClientsController < ApplicationController
   end
 
   def create
+    client = Client.find_by(email: params[:client][:email])
+    if params[:commit] == "SIGN IN"
+      if client && client.authenticate(params[:client][:password])
+        sign_in client
+        redirect_to reservations_path, notice: 'ログイン中'
+      else
+        redirect_to reservations_path, notice: '未登録です'
+      end
+    elsif params[:commit] == "SIGN UP"
+      if client
+        redirect_to new_client_path, notice: '登録済です'
+      else
+        Client.create!(
+          email: params[:client][:email],
+          password: params[:client][:password]
+        )
+        redirect_to reservations_path, notice: '登録しました'
+      end
+    end
   end
+
+  def destroy
+    sign_out
+    redirect_to reservations_path
+  end
+
+  private
+  # ログイン済みユーザーかどうか確認
+    def signed_in_client
+      unless signed_in?
+        redirect_to new_client_url
+      end
+    end
 end
