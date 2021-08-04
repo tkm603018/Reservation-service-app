744ad32571b7aed731ecc9194024eb797edf03b3
tkm603018 <tkm603018@gmail.com>
2021-08-04 13:57:48 +0900

    Update app/controllers/planners_controller.rb

--- app/controllers/planners_controller.rb
+++ app/controllers/planners_controller.rb
@@ -1,4 +1,7 @@
 class PlannersController < ApplicationController
+  include PlannersHelper
+  before_action :signed_in_planner, only:[:destroy]
+
   def index
   end
 
@@ -9,5 +12,37 @@ class PlannersController < ApplicationController
   end
 
   def create
+    planner = Planner.find_by(email: params[:planner][:email])
+    if params[:commit] == "サインイン"
+      if planner && planner.authenticate(params[:planner][:password])
+        sign_in planner
+        redirect_to reservation_frames_path, notice: 'ログイン中'
+      else
+        redirect_to reservation_frames_path, notice: '未登録です'
+      end
+    elsif params[:commit] == "SIGN UP"
+      if planner
+        redirect_to new_planner_path, notice: '登録済です'
+      else
+        Planner.create!(
+          email: params[:planner][:email],
+          password: params[:planner][:password]
+        )
+        redirect_to reservation_frames_path, notice: '登録しました'
+      end
+    end
   end
+
+  def destroy
+    sign_out
+    redirect_to reservation_frames_path
+  end
+
+  private
+  # ログイン済みユーザーかどうか確認
+    def signed_in_planner
+      unless signed_in?
+        redirect_to new_planner_url
+      end
+    end
 end
