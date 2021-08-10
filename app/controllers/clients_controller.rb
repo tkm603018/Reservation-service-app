7288afff0ab6d1600b5f0d6f9beb4e69d7b14411
tkm603018 <tkm603018@gmail.com>
2021-08-10 16:43:02 +0900

    Fix to early return

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -11,21 +11,17 @@ class ClientsController < ApplicationController
 
   def create
     client = Client.find_by(email: params[:client][:email])
-    if client
-      redirect_to new_client_path, notice: '登録済です'
+    return redirect_to new_client_path, alert: '登録済です' if client
+    client = Client.new(
+      email: params[:client][:email],
+      password: params[:client][:password],
+      password_confirmation: params[:client][:password_confirmation]
+    )
+    if client.valid?
+      client.save
+      redirect_to reservations_path, notice: '登録しました'
     else
-      client = Client.new(
-        email: params[:client][:email],
-        password: params[:client][:password],
-        password_confirmation: params[:client][:password_confirmation]
-      )
-      if client.valid?
-        client.save!
-        redirect_to reservations_path, notice: '登録しました'
-      else
-        redirect_to reservations_path, notice: client.errors.messages
-      end
+      redirect_to reservations_path, alert: client.errors.messages
     end
   end
-
 end
