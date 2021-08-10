e2964d84d5350bbc322c2dddf9dfc9aab1806927
tkm603018 <tkm603018@gmail.com>
2021-08-10 13:44:27 +0900

    Update app/controllers/clients_controller.rb

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,6 +1,4 @@
 class ClientsController < ApplicationController
-  include ClientsHelper
-  before_action :signed_in_client, only:[:destroy]
 
   def index
   end
@@ -13,42 +11,21 @@ class ClientsController < ApplicationController
 
   def create
     client = Client.find_by(email: params[:client][:email])
-    if params[:commit] == "サインイン"
-      if client && client.authenticate(params[:client][:password])
-        sign_in client
-        redirect_to reservations_path, notice: 'ログイン中'
+    if client
+      redirect_to new_client_path, notice: '登録済です'
+    else
+      client = Client.new(
+        email: params[:client][:email],
+        password: params[:client][:password],
+        password_confirmation: params[:client][:password_confirmation]
+      )
+      if client.valid?
+        client.save!
+        redirect_to reservations_path, notice: '登録しました'
       else
-        redirect_to reservations_path, notice: '未登録です'
-      end
-    elsif params[:commit] == "サインアップ"
-      if client
-        redirect_to new_client_path, notice: '登録済です'
-      else
-        client = Client.new(
-          email: params[:client][:email],
-          password: params[:client][:password],
-          password_confirmation: params[:client][:password_confirmation]
-        )
-        if client.valid?
-          client.save!
-          redirect_to reservations_path, notice: '登録しました'
-        else
-          redirect_to reservations_path, notice: client.errors.messages
-        end
+        redirect_to reservations_path, notice: client.errors.messages
       end
     end
   end
 
-  def destroy
-    sign_out
-    redirect_to reservations_path
-  end
-
-  private
-  # ログイン済みユーザーかどうか確認
-    def signed_in_client
-      unless signed_in?
-        redirect_to new_client_url
-      end
-    end
 end
