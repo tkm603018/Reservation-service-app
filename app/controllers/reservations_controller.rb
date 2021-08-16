a8915ae998baecf544a50c3ddf8cde79a2e2385b
tkm603018 <tkm603018@gmail.com>
2021-08-17 01:43:57 +0900

    メソッド(signed_in_client)を追加

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,7 +1,18 @@
 class ReservationsController < ApplicationController
+  before_action :signed_in_client, only: [:new, :create]
+
   def index
   end
 
   def new
+  end
+
+  private
+
+  def signed_in_client
+    unless signed_in?
+      flash[:alert] = "ログインするかユーザー登録してください"
+      redirect_to reservations_path
+    end
   end
 end
