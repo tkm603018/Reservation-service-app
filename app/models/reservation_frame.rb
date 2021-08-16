f75b59a3b6d999805b3cff2c22d688e14e765d5b
tkm603018 <tkm603018@gmail.com>
2021-08-17 01:45:55 +0900

    バリデーション(date_valid?, dow_valid?)を追加

--- app/models/reservation_frame.rb
+++ app/models/reservation_frame.rb
@@ -2,7 +2,18 @@ class ReservationFrame < ApplicationRecord
   belongs_to :planner
   belongs_to :time_frame
   
-  validates :planner_id, uniqueness: { scope: :time_frame_id }
-  
   has_many :reservations, dependent: :destroy
+
+  validates :planner_id, uniqueness: { scope: :time_frame_id }
+  validate :date_valid?
+  validate :dow_valid?
+
+  def date_valid?
+    errors.add(:reservation_frame, "日付を今日以降に設定してしてください") if reserved_date < Time.now
+  end
+
+  def dow_valid?
+    errors.add(:reservation_frame, "日曜日は休業日です") if reserved_date.strftime('%a') == 'Sun'
+  end
+  
 end
