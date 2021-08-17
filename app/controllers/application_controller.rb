feca16fdbd5f0363a133549b47d38dac801e93ab
tkm603018 <tkm603018@gmail.com>
2021-08-17 15:17:42 +0900

    メソッド(signed_in_client, signed_in_planner) を app/controllers/application_controller.rb へ移動

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
+  def signed_in_client
+    unless signed_in?
+      flash[:alert] = "ログインするかユーザー登録してください"
+      redirect_to reservations_path
+    end
+  end
+
+  def signed_in_planner
+    unless signed_in?
+      flash[:alert] = {user: "ログインするかユーザー登録してください"}
+      redirect_to reservation_frames_path
+    end
+  end
 end
