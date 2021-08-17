3a49cee083429494b1fee5017c5333734fd5b9cd
tkm603018 <tkm603018@gmail.com>
2021-08-17 15:31:15 +0900

    メソッド signed_in_client を削除

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,13 +6,4 @@ class ReservationsController < ApplicationController
 
   def new
   end
-
-  private
-
-  def signed_in_client
-    unless signed_in?
-      flash[:alert] = "ログインするかユーザー登録してください"
-      redirect_to reservations_path
-    end
-  end
 end
