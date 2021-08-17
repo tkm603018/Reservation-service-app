006f9baf3dde3eeaa657c318771b9cf3e884407c
tkm603018 <tkm603018@gmail.com>
2021-08-18 01:23:20 +0900

    メソッド def_time_frame をコントローラから移動

--- app/helpers/reservation_frames_helper.rb
+++ app/helpers/reservation_frames_helper.rb
@@ -1,2 +1,11 @@
 module ReservationFramesHelper
+
+  def def_time_frame
+    a = Time.local(2021, 1, 1, 10)
+    (0..15).map{|i| "#{t_format(a + 30.minute * i)}-#{t_format(a + 30.minute * (i+1))}"}
+  end
+
+  def t_format(x)
+    x.strftime("%H:%M")
+  end
 end
