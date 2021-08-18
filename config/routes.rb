b69d470a669b339ab78a30855ed57340d23436dc
tkm603018 <tkm603018@gmail.com>
2021-08-18 12:06:21 +0900

    reservations に メソッド create を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :reservations, only: [:index, :new]
+  resources :reservations, only: [:index, :new, :create]
   resources :reservation_frames, only: [:index, :new, :create]
   resources :clients, only: [:show, :new, :create]
   resources :planners, only: [:index, :show, :new, :create]
