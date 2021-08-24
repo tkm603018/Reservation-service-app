7fa2601cbfa04619aedac77655f99e70e3de59b6
tkm603018 <tkm603018@gmail.com>
2021-08-24 16:47:42 +0900

    index メソッドを記述(reservations_controller)

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,6 +2,7 @@ class ReservationsController < ApplicationController
   before_action :signed_in_user, only: [:new, :create]
 
   def index
+    @reservations = current_user && Reservation.where(client_id: current_user.id)
   end
 
   def new
