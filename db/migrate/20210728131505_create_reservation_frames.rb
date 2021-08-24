512082cc319cd248f6429d06784549874d067a58
tkm603018 <tkm603018@gmail.com>
2021-08-24 12:05:18 +0900

    status カラムをinteger型に変更

--- db/migrate/20210728131505_create_reservation_frames.rb
+++ db/migrate/20210728131505_create_reservation_frames.rb
@@ -1,9 +1,9 @@
 class CreateReservationFrames < ActiveRecord::Migration[6.1]
   def change
     create_table :reservation_frames do |t|
-      t.bigint :planner_id,       null: false, comment: "Plannerテーブルのid"
-      t.datetime :reserved_at,    null: false, comment: "予約日時"
-      t.string :status,           null: false, comment: "表示"
+      t.bigint :planner_id,                   null: false, comment: "Plannerテーブルのid"
+      t.datetime :reserved_at,                null: false, comment: "予約日時"
+      t.integer :status,          default: 0, null: false, comment: "表示"
 
       t.timestamps
     end
