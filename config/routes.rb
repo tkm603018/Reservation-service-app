64909470bb17554fa3907e974bb2f32e384ee082
tkm603018 <tkm603018@gmail.com>
2021-08-25 15:34:54 +0900

    新規アクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :reservations, only: [:index, :new, :create]
-  resources :reservation_frames, only: [:index, :new, :create]
+  resources :reservations, only: [:index, :new, :create, :destroy]
+  resources :reservation_frames, only: [:index, :new, :create, :destroy, :update]
   resources :clients, only: [:show, :new, :create]
   resources :planners, only: [:index, :show, :new, :create]
   resources :sessions, only: [:create, :destroy]
