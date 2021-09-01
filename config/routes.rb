3299cc14b48f55794064c7879faaa07e0e01d9c5
tkm603018 <tkm603018@gmail.com>
2021-09-01 12:20:05 +0900

    rootパスをクライアントの予約一覧に設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'reservations#index'
   resources :reservations, only: [:index, :new, :create, :destroy]
   resources :reservation_frames, only: [:index, :new, :create, :destroy, :update]
   resources :clients, only: [:show, :new, :create]
