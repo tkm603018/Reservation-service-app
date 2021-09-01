fe09b5410f47561f5fda88838ed555bd8a3fe192
tkm603018 <tkm603018@gmail.com>
2021-09-01 13:20:26 +0900

    ダブル -> シングル クォーテーションへ修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def signed_in_user
     unless signed_in?
-      flash[:alert] = "サインインするか登録してください"
+      flash[:alert] = 'サインインするか登録してください'
       if params[:controller] == "reservation_frames"
         redirect_to reservation_frames_path
       elsif params[:controller] == "reservations"
